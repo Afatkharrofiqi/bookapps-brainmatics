2bf29f351f7dcfeec13ae1ab440f10e952ca452a
achmad <achmadfatkharrofiqi404@gmail.com>
2022-09-22 09:18:45 +0700

    feat: list of category

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">List of Category</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead class="table-dark">
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Created At</th>
+                                <th>Updated At</th>
+                                <th>Action</th>
+                            </thead>
+                            <tbody>
+                                @forelse ($categories as $key => $category)
+                                    <tr>
+                                        <td>{{$categories->firstItem() + $key}}</td>
+                                        <td>{{$category->name}}</td>
+                                        <td>{{$category->created_at}}</td>
+                                        <td>{{$category->updated_at}}</td>
+                                        <td class="justify-content-between">
+                                            <a href="#" class="btn btn-primary">Edit</a>
+                                            <a href="#" class="btn btn-danger">Delete</a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5">Category is empty</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $categories->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
