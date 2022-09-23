a27f40eb19893135f3d1355ea1077b5c3c22a683
aryo <aryofaqih@gmail.com>
2022-09-23 14:57:32 +0700

    refc: update bookstore

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -5,19 +5,27 @@ namespace App\Services;
 use App\Exceptions\CategoryStoreException;
 use App\Models\Book;
 use App\Services\Interfaces\BookInterface;
+use Illuminate\Support\Facades\DB;
 
 class BookService implements BookInterface {
 
     public function store(array $data)
     {
+        DB::beginTransaction();
         try {
-            $category = new Book;
-            $category->name = $data['name'];
-            $category->save();
+            $book = new Book();
+            $book->title = $data['title'];
+            $book->cover = $data['cover']->file('cover')->store('book-cover', 'public');
+            $book->year = $data['year'];
+            $book->created_by = auth()->user()->id;
+            $book->save();
 
-            return $category;
-        } catch (\Exception $th) {
-            throw new CategoryStoreException($th->getMessage());
+            $book->categories()->attach($data['categories']);
+            DB::commit();
+            return redirect()->route('book.index')->with('message-success', 'Book created successfully');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('book.index')->with('message-fail', 'Book create failure .'.$e->getMessage());
         }
     }
 
